339dd539a12584c70b3f297ea09ac2fc08fbcf21
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-11-12 09:24:10 -0300

    a

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -107,7 +107,7 @@ class MainMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(0, (i * 120)  + offset);
+			var menuItem:FlxSprite = new FlxSprite(0, (i * 90)  + offset);
 			menuItem.scale.x = scale;
 			menuItem.scale.y = scale;
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/' + optionShit[i]);
